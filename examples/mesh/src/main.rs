607c71fdd6bf47af52b287191772ce5f2a2033d0
IcyDefiance <icydefiance@gmail.com>
2018-06-24 14:07:35 -0400

    Hardcode a directional light

--- examples/mesh/src/main.rs
+++ examples/mesh/src/main.rs
@@ -76,7 +76,7 @@ fn make_camera(window: &Window) -> Camera {
 	let [width, height] = window.images()[0].dimensions().width_height();
 	Camera::new(
 		&window,
-		vec3(-5.0, -7.0, -15.0),
+		vec3(-5.0, -7.0, -19.0),
 		Quaternion::one(),
 		width as f32 / height as f32,
 		100.0,
